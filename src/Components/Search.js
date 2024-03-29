7d79fd73b31be33d28abb6ff818d31d875d47909
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-03-29 10:05:54 +0530

    fix the name changes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,36 +1,15 @@
-import React, { useEffect } from "react";
+import React from 'react'
+import { useGlobalContext } from "./Context";
+
 
 const Search = () => {
-  let isLoading = true;
-  let API = "https://hn.algolia.com/api/v1/search?query=html";
-
-  const fetchApiData = async (url) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-      //  isLoading = false;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchApiData(API);
-  }, []);
-
-  if (!isLoading) {
-    return (
-      <>
-        <h1>Loading...</h1>
-      </>
-    );
-  }
+  const data=useGlobalContext()
   return (
-    <>
-      <h2>Tech News Post</h2>
-    </>
-  );
-};
+    <div>
+      <h5>Search : :{data}</h5>
+    </div>
+  )
+}
 
-export default Search;
+export default Search
+
--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import { useGlobalContext } from "./Context";
-
+import React, { useEffect } from "react";
 
 const Stories = () => {
-  const data=useGlobalContext()
-  return (
-    <div>
-      <h5>Strores : :{data}</h5>
-    </div>
-  )
-}
+  let isLoading = true;
+  let API = "https://hn.algolia.com/api/v1/search?query=html";
 
-export default Stories
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      //  isLoading = false;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
+  return (
+    <>
+      <h2>Tech News Post</h2>
+    </>
+  );
+};
+
+export default Stories;
